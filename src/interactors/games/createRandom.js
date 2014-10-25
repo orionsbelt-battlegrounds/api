d66035015329762538cb95dfa779a659c478e6a4
Pedro Santos <donbonifacio@gmail.com>
2014-10-25 17:51:00 +0100

    Add async module

--- src/interactors/games/createRandom.js
+++ src/interactors/games/createRandom.js
@@ -3,6 +3,7 @@ var config = require("./../../../src/config.js");
 var result = require("./../../../src/logic/result.js");
 var auth = require("./../../../src/logic/auth.js");
 var playerGateway = require("./../../../src/gateways/player.js");
+var async = require(async);
 
 function run(args) {
   var tokenResult = auth.validateToken(args);
